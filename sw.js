b50ca370081c13c8efa70f9c03577b7203dfffa5
greychateg <chen15651623522@gmail.com>
2026-03-05 16:08:11 +0800

    update sw.js: network-first + v6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='xz-v5';
-const URLS=['/','index.html'];
+const CACHE='xz-v6';
+const URLS=['./','./index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{if(res.ok){const c=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{if(res.ok){const c=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
